c9f59b999c19c4180c3bbfad55c85a5cc37fcd0c
svelezw4 <santiagovelezw4@santiagacstudio.lan>
2024-09-17 13:06:14 -0600

    Add multiply

--- summator.cpp
+++ summator.cpp
@@ -10,6 +10,10 @@ void Summator::substract(int p_value) {
 	count -= p_value;
 }
 
+void Summator::multiply(int p_value) {
+	count *= p_value;
+}
+
 void Summator::reset() {
 	count = 0;
 }
@@ -21,6 +25,7 @@ int Summator::get_total() const {
 void Summator::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("add", "value"), &Summator::add);
 	ClassDB::bind_method(D_METHOD("substract", "value"), &Summator::substract);
+	ClassDB::bind_method(D_METHOD("multiply", "value"), &Summator::multiply);
 	ClassDB::bind_method(D_METHOD("reset"), &Summator::reset);
 	ClassDB::bind_method(D_METHOD("get_total"), &Summator::get_total);
 }
