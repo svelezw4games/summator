74c4bde06905f20f2dad59c3c1b74804c7da8afd
svelezw4 <santiagovelezw4@santiagacstudio.lan>
2024-09-17 12:51:27 -0600

    Add substract method

--- summator.cpp
+++ summator.cpp
@@ -6,6 +6,10 @@ void Summator::add(int p_value) {
 	count += p_value;
 }
 
+void Summator::substract(int p_value) {
+	count += p_value;
+}
+
 void Summator::reset() {
 	count = 0;
 }
@@ -16,6 +20,7 @@ int Summator::get_total() const {
 
 void Summator::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("add", "value"), &Summator::add);
+	ClassDB::bind_method(D_METHOD("substract", "value"), &Summator::substract);
 	ClassDB::bind_method(D_METHOD("reset"), &Summator::reset);
 	ClassDB::bind_method(D_METHOD("get_total"), &Summator::get_total);
 }
